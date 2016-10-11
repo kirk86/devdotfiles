d34f124a327f401c112540da854875b86c82d3eb
kirk86 <kirk86@walla.com>
2016-10-11 20:46:21 +0100

    removing exit that halted operation

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -55,11 +55,10 @@ function install_tigervnc
     sudo adduser tigervncuser;
     # passwd tigervncuser
 
-    read -p "Do you wanna add tigervncuser to sudoers group ? [y/N]" yn
+    read -p "Do you wanna add tigervncuser to sudoers group ? [y/N] " yn
     case $yn in
         Y|y ) sudo usermod -aG sudo tigervncuser;;
-        * ) echo "Yes master!"
-            exit 0
+        * ) echo "Yes master!";;
     esac
 
     su - tigervncuser;
