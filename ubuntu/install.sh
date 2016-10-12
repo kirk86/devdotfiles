822a273abe569be8d39f2333d281b0f53ed13fae
kirk86 <kirk86@walla.com>
2016-10-12 11:23:43 +0100

    new changes

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,12 +1,12 @@
 function install_prerequisites
 {
     # update packages
-    sudo aptg-get update
-    sudo apt-get upgrade
-    sudo apt-get dist-ugrade
+    sudo aptg-get update;
+    sudo apt-get upgrade;
+    sudo apt-get dist-ugrade;
 
     # remove unecessary packages
-    sudo apt-get autoremove
+    sudo apt-get autoremove;
 
     # install prerequisites
     sudo apt-get install -y software-properties-common \
@@ -26,13 +26,13 @@ function install_prerequisites
 function install_tigervnc
 {
     # install minimal gui for vnc/rdp access
-    echo "Installing minimal gui."
-    sudo apt-get install -y xorg lxde-core lxtask lxrandr lxterminal git \
-         devscripts lxde-common xinit lsb-core lsb_release
+    echo "Installing minimal gui.";
+    sudo apt-get install -y xorg lxde-core lxtask lxrandr lxterminal \
+         devscripts lxde-common xinit lsb-core lsb_release git
 
-    mkdir -p ${HOME}/tigervnc && cd ${HOME}/tigervnc
-    git clone https://github.com/TigerVNC/tigervnc
-    cd tigervnc
+    mkdir -p ${HOME}/tigervnc && cd ${HOME}/tigervnc;
+    git clone https://github.com/TigerVNC/tigervnc;
+    cd tigervnc;
     git checkout 044e2b87da7121ef6cbd59e88b101d7d8e282896 \
         -b 044e2b87da7121ef6cbd59e88b101d7d8e282896
     # ln -s contrib/packages/deb/ubuntu-$(lsb_release -a | grep -i codename | cut -f2)/debian
@@ -42,37 +42,37 @@ function install_tigervnc
     sudo apt install -y $(grep Build-Depends: debian/control | \
                                  sed -e 's/Build-Depends://g' -e 's/([^\)]*)//g' -e 's/,//g')
     # build tigervnc
-    chmod a+x debian/rules
-    fakeroot debian/rules binary
+    chmod a+x debian/rules;
+    fakeroot debian/rules binary;
 
     # Install deb files while installing depend package with running
     # "apt -f install".
-    cd ..
-    sudo dpkg -i *.deb || (sudo apt -f install -y; sudo dpkg -i *.deb)
-    cd ..
+    cd ..;
+    sudo dpkg -i *.deb || (sudo apt -f install -y; sudo dpkg -i *.deb);
+    cd ..;
 
     # create vncuser
     echo "Adding user for tigervncserver with username tigervcnuser";
     sudo adduser tigervncuser;
     # passwd tigervncuser
 
-    read -p "Do you wanna add tigervncuser to sudoers group ? [y/N] " yn
+    read -p "Do you wanna add tigervncuser to sudoers group ? [y/N] " yn;
     case $yn in
         Y|y ) sudo usermod -aG sudo tigervncuser;;
-        * ) echo "Yes master!"
+        * ) echo "Yes master!";
     esac
 
-    echo "Switching to vncserver user in order to add vncserver password!"
-    sudo runuser -l tigervncuser -c tigervncpasswd
+    echo "Switching to vncserver user in order to add vncserver password!";
+    sudo runuser -l tigervncuser -c tigervncpasswd;
     # sudo -H -u tigervncuser bash -c tigervncpasswd
 
     # run vncserver once to create config files and kill
-    echo "Running tigervncserver in order to create config files."
-    tigervncserver
-    echo "Killing tigervncserver."
-    tigervncserver -kill :1
+    echo "Running tigervncserver in order to create config files.";
+    tigervncserver;
+    echo "Killing tigervncserver.";
+    tigervncserver -kill :1;
 
-    cp ${HOME}/.vnc/xstartup ${HOME}/.vnc/xstartup-old
+    cp ${HOME}/.vnc/xstartup ${HOME}/.vnc/xstartup-old;
 
    cat <<EOF > ${HOME}/.vnc/xstartup
 #!/bin/sh
@@ -91,24 +91,24 @@ startlxde &
 EOF
 
    # install and run xrdp server
-   sudo apt install -y xrdp
-   sudo systemctl enable xrdp
-   sudo systemctl restart xrdp
+   sudo apt install -y xrdp;
+   sudo systemctl enable xrdp;
+   sudo systemctl restart xrdp;
 
    # finally enable tigervnc
-   tigervncserver
+   tigervncserver;
 
    # cleanup
-   rm -rf ${HOME}/tigervnc
+   rm -rf ${HOME}/tigervnc;
 }
 
 function install_openblas
 {
     # Get and build OpenBlas (Torch is much better with a decent Blas)
-    cd /tmp/
-    rm -rf OpenBLAS
-    git clone https://github.com/xianyi/OpenBLAS.git
-    cd OpenBLAS
+    cd /tmp/;
+    rm -rf OpenBLAS;
+    git clone https://github.com/xianyi/OpenBLAS.git;
+    cd OpenBLAS;
     if [ $(getconf _NPROCESSORS_ONLN) == 1 ]; then
         make NO_AFFINITY=1 USE_OPENMP=0 USE_THREAD=0
     else
@@ -119,37 +119,39 @@ function install_openblas
         echo "Error. OpenBLAS could not be compiled";
         exit $RET;
     fi
-    sudo make install
+    sudo make install;
     RET=$?;
     if [ $RET -ne 0 ]; then
         echo "Error. OpenBLAS could not be installed";
         exit $RET;
     fi
 
-    echo "Cleaning up!"
-    rm -rf /tmp/OpenBLAS
+    echo "Cleaning up!";
+    rm -rf /tmp/OpenBLAS;
 }
 
 function install_anaconda
 {
     # download and install conda
-    cd ${HOME}
+    cd ${HOME};
 
-    read -p "Do you wanna install miniconda or anaconda ? [y/N] " yn
+    read -p "Do you wanna install miniconda or anaconda ? [y/N] " yn;
     case $yn in
         Y|y ) wget https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh;
               bash Miniconda2-4.1.11-Linux-x86_64.sh;
+              source ${HOME}/.bashrc;
               read -p "Do you wanna install Theano ? [y/N] " yn;
               case $yn in
                   Y|y ) pip install Theano;;
-                  * ) echo "Yes master!"
+                  * ) echo "Yes master!";
               esac;;
         * ) wget https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh;
             bash Anaconda2-4.2.0-Linux-x86_64.sh;
+            source ${HOME}/.bashrc;
             read -p "Do you wanna install Theano ? [y/N] " yn;
             case $yn in
                 Y|y ) conda install Theano;;
-                * ) echo "Yes master!"
+                * ) echo "Yes master!";
             esac
     esac
 
@@ -158,21 +160,26 @@ function install_anaconda
     # echo export PATH=$HOME/anaconda/bin:$PATH >> ${HOME}/.bashrc
 
 
-    read -p "Would you like to run the tests for Theano now ? [y/n] " yn
+    read -p "Would you like to run the tests for Theano now ? [y/n] " yn;
     case $yn in
-        y|y ) python -c "import theano; theano.test()";;
-        * ) echo "yes master!"
+        y|y ) conda install nose;
+              pip install nose-parameterized;
+              sudo apt-get install graphviz;
+              conda install graphviz;
+              conda install pydot;
+              python -c "import theano; theano.test()";;
+        * ) echo "yes master!";
     esac
 
-    read -p "Do you wanna install Torch7 separetely or as part of anaconda ? [y/N] " yn
+    read -p "Do you wanna install Torch7 separetely or as part of anaconda ? [y/N] " yn;
     case $yn in
         Y|y ) git clone https://github.com/torch/distro.git ~/torch --recursive;
               cd ~/torch; bash install-deps; ./install.sh;
-              read -p "would you like to run the tests for Torch7 now ? [y/n] " yn
+              read -p "would you like to run the tests for Torch7 now ? [y/n] " yn;
               case $yn in
                   Y|y ) cd ${HOME}/torch; bash ./test.sh;;
-                  * ) echo "yes master!"
+                  * ) echo "yes master!";
               esac;;
-        * ) conda install lua=5.2 lua-science -c alexbw
+        * ) conda install lua=5.2 lua-science -c alexbw;
     esac
 }
