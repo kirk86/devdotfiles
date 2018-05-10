72709d0a857cadbee3a1f0bfde83974018800041
kirk86 <kirk86@walla.com>
2018-05-10 23:51:02 +0100

    Update conky-startup.sh

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,4 +1,4 @@
 sleep 20s
 killall conky
-cd "/home/user/.conky/Green Apple Desktop"
-conky -c "/home/user/.conky/Green Apple Desktop/conky_seamod" &
+cd "/home/$(whoami)/.conky/Green Apple Desktop"
+conky -c "/home/$(whoami)/.conky/Green Apple Desktop/conky_seamod" &
